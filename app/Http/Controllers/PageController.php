d72bb72931d23cc1a3348af598b261daf872186d
Tran Hoang Xuan Nguyen <hoangnguyendepgiai@gmail.com>
2025-03-11 09:38:46 +0700

    done

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Slide;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 
@@ -10,6 +11,9 @@ class PageController extends Controller
 {
     public function index(){
         $slide = Slide::all();
-        return view('pages.index', ['slide' => $slide]);
+        $products = Product::where('id_type', 6)->limit(4)->get();
+        $product2 = Product::where('id_type', 5)->limit(4)->get();
+        $product3 = Product::where('id_type', 4)->limit(4)->get();
+        return view('pages.trangchu', compact('slide', 'products', 'product2', 'product3'));
     }
 }
